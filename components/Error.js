d3a7d27f92306da669b612ec42c0c96fbe0e18f2
brian <118747961+sprillx@users.noreply.github.com>
2023-02-04 22:50:35 -0500

    Add files via upload

---
+++ components/Error.js
@@ -0,0 +1,13 @@
+import { Col } from "reactstrap";
+
+
+const Error = ({ errMsg }) => {
+    return (
+        <Col>
+            <h4>{errMsg}</h4>
+        </Col>
+        )
+}
+
+
+export default Error 
